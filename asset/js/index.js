1bd3de23d1ab981c45da1d32bc9d9153f34c27a1
turkana78 <turkanayar@gmail.com>
2018-01-31 19:08:04 +0300

    index.js

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,6 +1,6 @@
 
 Vue.component('i18n_custom',{
-		
+
 	template : '<select v-model="lang"  v-on:change="changeLanguage">'
 	+'<option value="en">{{ $t("message.english") }}</option>'
 	+'<option value="tr">{{ $t("message.turkish") }}</option>'
@@ -21,7 +21,7 @@ Vue.component('i18n_custom',{
 
 if(!window.localStorage.getItem("lang"))
 		window.localStorage.setItem("lang", "tr");
-		
+
 var i18n = new VueI18n({
   locale: window.localStorage.getItem("lang"), // set locale
   messages:messages // set locale messages
@@ -34,7 +34,7 @@ Vue.component('entity',{
 						+'<h3>{{index}} - {{name}} </h3>'
 						+'<p><input type="text" v-model="sentence"></p>'
 						+'<p><label>{{$t("message.storedSentence")}} : </label><span>{{storedSentence}}</span></p>'
-						+'<p><a  class="btn btn-primary" role="button" v-on:click="removeSentece(id)">{{$t("message.remove")}}</a>' 
+						+'<p><a  class="btn btn-primary" role="button" v-on:click="removeSentece(id)">{{$t("message.remove")}}</a>'
 						+'<a class="btn btn-default" role="button" v-on:click="addSentence(id)">{{$t("message.add")}}</a></p>'
 					  +'</div>'
 					+'</div>'
@@ -56,15 +56,15 @@ Vue.component('entity',{
 	},
 	data :	function () {
 		return {sentence : "",storedSentence : ""}
-		
+
 	}
-				  
+
 });
 
 Vue.component('row',{
 	template : '<div class="row"> <entity v-for="(entity,index) in array" v-bind:name="entity.name" v-bind:id="entity.id"  v-bind:index="index" :key="entity.id"></entity></div>',
 	props: ['array']
-	
+
 });
 
 var container = Vue.component('container',{
@@ -81,9 +81,12 @@ var container = Vue.component('container',{
 							+'</span>'
 						+'</div>'
 				+'</div>'
-				+'<div class="content"><div><label>{{$t("message.search")}}</label> <input type="text" v-model="searchText" v-on:keyup="search"/></div><br/><br/><row v-for="entityArray in this.entityList"  v-bind:array="entityArray" ></row></div>'
+				+'<div class="content"><div style="width:20%;display:inline-block"><ul v-for="intent in this.original"><li v-for="i in intent"><span v-on:click="showOnlyThisItem(i)">{{i.id}} - {{i.name}}<span></li></ul></div><div style="width:80%;display:inline-block;vertical-align: top;" ><div ><label>{{$t("message.search")}}</label> <input type="text" v-model="searchText" v-on:keyup="search"/></div><br/><br/><row v-for="entityArray in this.intentList"  v-bind:array="entityArray" ></row></div></div>'
 				+'<div class="footer"></div></div>',
 	methods : {
+		showOnlyThisItem : function(entity){
+			this.intentList=[[entity]];
+		},
 		search : function(){
 			if(this.searchText.trim() == ""){
 				this.immutableObjectToEntity();
@@ -94,17 +97,17 @@ var container = Vue.component('container',{
 				for(var j = 0 ; j < this.original[i].length;j++){
 					if(this.original[i][j].name.toLocaleUpperCase().indexOf(this.searchText.toLocaleUpperCase()) < 0){
 						var k = 0 ;
-						for(k=0 ; k < this.entityList.length;k++){
+						for(k=0 ; k < this.intentList.length;k++){
 							var z = 0 ;
 							var flag = false;
-							for(z = 0 ; z < this.entityList[k].length; z++){
-								if(this.entityList[k][z].id == this.original[i][j].id){
+							for(z = 0 ; z < this.intentList[k].length; z++){
+								if(this.intentList[k][z].id == this.original[i][j].id){
 									flag= true;
 									break;
 								}
 							}
 							if(flag){
-								this.entityList[k].splice(z,1);
+								this.intentList[k].splice(z,1);
 							}
 						}
 					}
@@ -112,21 +115,21 @@ var container = Vue.component('container',{
 			}
 		},
 		immutableObjectToOriginal: function(){
-			for(var i = 0 ; i < this.entityList.length ;i++){
+			for(var i = 0 ; i < this.intentList.length ;i++){
 				var mod3Array = [];
-				for(var j = 0 ; j < this.entityList[i].length;j++){
+				for(var j = 0 ; j < this.intentList[i].length;j++){
 					var obj = {};
-					for(var key in this.entityList[i][j]){
-						obj[key] = this.entityList[i][j][key];
+					for(var key in this.intentList[i][j]){
+						obj[key] = this.intentList[i][j][key];
 					}
 					mod3Array = mod3Array.concat(obj);
 				}
-				
+
 				this.original.push(mod3Array);
 			}
 		},
 		immutableObjectToEntity: function(){
-			this.entityList = [];
+			this.intentList = [];
 			for(var i = 0 ; i < this.original.length ;i++){
 				var mod3Array = [];
 				for(var j = 0 ; j < this.original[i].length;j++){
@@ -136,23 +139,23 @@ var container = Vue.component('container',{
 					}
 					mod3Array = mod3Array.concat(obj);
 				}
-				
-				this.entityList.push(mod3Array);
+
+				this.intentList.push(mod3Array);
 			}
 		}
-			
-			
+
+
 	},
 	mounted : function(){
 		this.$nextTick(function () {
 			this.immutableObjectToOriginal();
 		})
-		
+
 	},
 	data :	function () {
-		return {entityList :[[{id : 1 ,name : "A İntent"},{id : 2 ,name : "B Intent"},{id : 3 ,name : "C Intent"}],[{id : 4  ,name : "D Intent"},{id : 5 ,name:"E Intent"}]], original :[],
+		return {intentList :[[{id : 1 ,name : "A İntent"},{id : 2 ,name : "B Intent"},{id : 3 ,name : "C Intent"}],[{id : 1 ,name : "A İntent"},{id : 2 ,name : "B Intent"},{id : 3 ,name : "C Intent"}],[{id : 1 ,name : "A İntent"},{id : 2 ,name : "B Intent"},{id : 3 ,name : "C Intent"}],[{id : 1 ,name : "A İntent"},{id : 2 ,name : "B Intent"},{id : 3 ,name : "C Intent"}],[{id : 1 ,name : "A İntent"},{id : 2 ,name : "B Intent"},{id : 3 ,name : "C Intent"}],[{id : 1 ,name : "A İntent"},{id : 2 ,name : "B Intent"},{id : 3 ,name : "C Intent"}],[{id : 1 ,name : "A İntent"},{id : 2 ,name : "B Intent"},{id : 3 ,name : "C Intent"}],[{id : 1 ,name : "A İntent"},{id : 2 ,name : "B Intent"},{id : 3 ,name : "C Intent"}],[{id : 1 ,name : "A İntent"},{id : 2 ,name : "B Intent"},{id : 3 ,name : "C Intent"}],[{id : 1 ,name : "A İntent"},{id : 2 ,name : "B Intent"},{id : 3 ,name : "C Intent"}],[{id : 1 ,name : "A İntent"},{id : 2 ,name : "B Intent"},{id : 3 ,name : "C Intent"}],[{id : 1 ,name : "A İntent"},{id : 2 ,name : "B Intent"},{id : 3 ,name : "C Intent"}],[{id : 4  ,name : "D Intent"},{id : 5 ,name:"E Intent"}]], original :[],
 		searchText : ""}
-		
+
 	}
 });
 var vrouter = new VueRouter({
@@ -165,17 +168,17 @@ var vue = new Vue({
   el: '#app',
   data: {
   },
-  methods :{	
+  methods :{
   }
   ,
 	mounted: function () {
 	  this.$nextTick(function () {
-		 
+
 	  })
 	}
   ,
   methods : {},
   router : vrouter,
   i18n : i18n
-  
-});	
+
+});
